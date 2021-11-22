a591635f6b8157cb1816a329f08f82e7dbce8c5a
雨太阳 <50984404+goodluckily@users.noreply.github.com>
2021-11-22 21:30:45 +0800

    Update Startup.cs

--- WeChat.Host/Startup.cs
+++ WeChat.Host/Startup.cs
@@ -9,12 +9,14 @@ namespace WeChat.Host
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //模块化
             services.AddApplication<WeChatHostModule>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            //
             app.InitializeApplication();
         }
     }
