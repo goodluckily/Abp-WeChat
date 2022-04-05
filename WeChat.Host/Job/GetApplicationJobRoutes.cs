da5e6c85199f5c094714b45efed27625ee480e6f
goodluckily <15652338313@163.com>
2022-04-05 21:53:11 +0800

    Api方式的Job调用

---
+++ WeChat.Host/Job/GetApplicationJobRoutes.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using WeChat.Application;
+
+namespace WeChat.Host.Job
+{
+    public class GetApplicationJobRoutes
+    {
+        /// <summary>
+        /// 获取api下的job任务路由
+        /// </summary>
+        /// <returns></returns>
+        public static List<string> GetJobRoutes()
+        {
+            var JobRouterList = new List<string>();
+
+            #region new版本
+            //1.反射得到所有 定时任务类
+            var assembly = Assembly.Load("WeChat.Application");
+            var types = assembly.GetTypes().Where(x => x.Namespace == "WeChat.Application.Services.Job");
+            var asdfas = types.Select(s => s.GetCustomAttributes()).ToList();
+            //包含次特性的类
+            var classArray = types.Where(s => s.GetMethods().Any(x => x.CustomAttributes.Any(y => y.AttributeType == typeof(BathBackgroundJobAttribute)))).ToList();
+
+            foreach (var t in classArray)
+            {
+                //获取方法特性中ActionName为GetSchoolAll的特性
+                var jobMethods = t.GetMethods().Where(s => s.CustomAttributes.Any(x => x.AttributeType == typeof(BathBackgroundJobAttribute))).ToList();
+                if (!jobMethods.Any()) continue;
+
+                var routeName = t.GetCustomAttributes();
+                var ControlRouteAttribute = t.CustomAttributes.FirstOrDefault(x => x.AttributeType.Name == "RouteAttribute");
+                //1.得到service(控制器的)路由名称没有就是类名称
+                var RouteControlName = ControlRouteAttribute != null ? ControlRouteAttribute.ConstructorArguments[0].Value?.ToString() : t.Name;
+                var methodsData = new List<string>();
+                foreach (var item in jobMethods)
+                {
+                    var RouteMethedName = item.CustomAttributes.FirstOrDefault(x => x.AttributeType == typeof(HttpPostAttribute));
+                    //2.得到方法的路由名称,没有就是方法名称
+                    var MethedName = RouteMethedName != null ? RouteMethedName.ConstructorArguments[0].Value?.ToString() : item.Name;
+                    JobRouterList.Add($"{RouteControlName}/{MethedName}");
+                }
+            }
+            #endregion
+
+            return JobRouterList;
+        }
+    }
+}
--- WeChat.Host/WorkService.cs
+++ WeChat.Host/WorkService.cs
@@ -10,6 +10,13 @@ using WeChat.Http.HttpHelper;
 using System.Net.Http;
 using WeChat.Shared;
 using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using WeChat.Application;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using WeChat.Host.Job;
 
 namespace WeChat.Host
 {
@@ -17,50 +24,91 @@ namespace WeChat.Host
     {
         private readonly ILogger<WorkService> _logger;
         private readonly IServiceProvider _provider;
-        private readonly IHostEnvironment _hostEnvironment;
         private readonly HttpClientHelper httpClientHelper;
+        private readonly string JobHostAddress = "";
 
-        public WorkService(ILogger<WorkService> logger, IServiceProvider serviceProvider, IWebHostEnvironment hostEnvironment)
+        public WorkService(ILogger<WorkService> logger, IServiceProvider serviceProvider)
         {
-            var aa = ConfigCommon.Configuration["urls"];
             _logger = logger;
             _provider = serviceProvider;
             httpClientHelper = new HttpClientHelper();
+            JobHostAddress = ConfigCommon.Configuration["JobHostAddress"];
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             _logger.LogInformation("WorkService is starting.");
+
+            #region old版本 注释 !!!
+            //while (!stoppingToken.IsCancellationRequested)
+            //{
+            //    _logger.LogInformation($"WorkService is running.{DateTime.Now}");
+
+            //    #region MyRegion
+            //    //do something
+            //    //RecurringJob.AddOrUpdate("Test", () => Console.WriteLine($"Test is running.{DateTime.Now}"), Cron.Minutely, timeZone: TimeZoneInfo.Local);
+            //    //RecurringJob.AddOrUpdate("CodeDeaultblogsContent", () => _codeDeaultblogsJob.CodeDeaultblogsContent().Wait(), Cron.Daily(18, 40), TimeZoneInfo.Local);
+            //    //await BathHangfireJobAsync(); 
+            //    #endregion
+
+            //    #region Old
+            //    //动态
+            //    //dynamic configuration = _provider.GetRequiredService(typeof(CodeDeaultblogsJob));
+            //    //这里可能需要反射
+            //    //await configuration.CodeDeaultblogsContent(); 
+            //    #endregion
+
+            //    //循环检查激活/刷新的意思 这个以后改成配置文件的时候用到
+            //    await Task.Delay(new TimeSpan(0, 30, 0), stoppingToken);
+            //}
+            #endregion
+
+            #region 模拟
+            ////可以之后的这个是可以的 哈哈 url请求数据
+            //RecurringJob.AddOrUpdate("baidu", () => Console.WriteLine("baidu"), "0 */2 * * * ?", TimeZoneInfo.Local);// 每隔2分钟执行一次
+            //RecurringJob.AddOrUpdate("tengxun", () => Console.WriteLine("tengxun"), "0 */3 * * * ?", TimeZoneInfo.Local);// 每隔3分钟执行一次 
+            #endregion
+
+            var routeJobs = GetApplicationJobRoutes.GetJobRoutes();
+            var nowTime = DateTime.Now;
+            var startTime = new DateTime(nowTime.Year, nowTime.Month, nowTime.Day, 19, 30, 0);
+            var endTime = new DateTime(nowTime.Year, nowTime.Month, nowTime.Day, 20, 30, 0);
+            //批量添加Job任务
             while (!stoppingToken.IsCancellationRequested)
             {
-
-                // do something
-                _logger.LogInformation($"WorkService is running.{DateTime.Now}");
-
-                //RecurringJob.AddOrUpdate("Test", () => Console.WriteLine($"Test is running.{DateTime.Now}"), Cron.Minutely, timeZone: TimeZoneInfo.Local);
-                //RecurringJob.AddOrUpdate("CodeDeaultblogsContent", () => _codeDeaultblogsJob.CodeDeaultblogsContent().Wait(), Cron.Daily(18, 40), TimeZoneInfo.Local);
-
-                //await BathHangfireJobAsync();
-
-                //可以之后的这个是可以的 哈哈 url请求数据
-                RecurringJob.AddOrUpdate("httpCodeDeaultblogsContent", () => Test(), Cron.Daily(23, 55), TimeZoneInfo.Local);
-
-                #region OK
-                //动态
-                //dynamic configuration = _provider.GetRequiredService(typeof(CodeDeaultblogsJob));
-                //这里可能需要反射
-                //await configuration.CodeDeaultblogsContent(); 
-                #endregion
-
-                //循环检查激活/刷新的意思
-                await Task.Delay(new TimeSpan(0, 30, 0), stoppingToken);
+                foreach (var jobPath in routeJobs)
+                {
+                    //获取分钟时间
+                    var randomTim = GetRandomTime(startTime, endTime);
+                    _logger.LogWarning("aa============" + randomTim.ToString());
+                    RecurringJob.AddOrUpdate(jobPath, () => PostApiRequestJobService(jobPath), Cron.Daily(randomTim.Hour, randomTim.Minute), TimeZoneInfo.Local);
+                }
+                _logger.LogInformation($"WorkService is UpDate.{DateTime.Now}");
+                //循环检查激活/刷新的意思 这个以后改成配置文件的时候用到
+                await Task.Delay(new TimeSpan(1, 0, 0), stoppingToken);
             }
             _logger.LogInformation("WorkService is stopping.");
         }
 
-        public void Test()
+        /// <summary>
+        /// Api的方式请求Job里面的ApiService
+        /// </summary>
+        /// <param name="servicePath"></param>
+        public void PostApiRequestJobService(string servicePath)
         {
-            var result = httpClientHelper.PostResponse("http://127.0.0.1:5555/CodeDeaultblogsJob/CodeDeaultblogsContent", "");
+            var loadJobUrl = JobHostAddress + servicePath;
+            var result = httpClientHelper.PostResponse(loadJobUrl, "");
+            _logger.LogInformation($"{servicePath}-------->{result}");
+        }
+
+        //获取指定时间范围内的随机时间
+        public DateTime GetRandomTime(DateTime startTime, DateTime endTime)
+        {
+            var random = new Random();
+            var startInt = (int)startTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            var endInt = (int)endTime.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            var randomInt = random.Next(startInt, endInt);
+            return new DateTime(1970, 1, 1).AddSeconds(randomInt);
         }
     }
 }
